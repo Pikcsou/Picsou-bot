6bc5fb5470adf5666058ef9634ca42d0ac576beb
Cyanic <hi.plexify@null.net>
2020-11-27 18:55:22 +0100

    Optimisation + Correction
    Voir pull request

--- documents système/index.js
+++ documents système/index.js
@@ -1,52 +1,50 @@
   
-const  Discord  =  require ( "discord.js" ) ;
- client  const =  nouveau  Discord . Client ( ) ;
-const  fs  =  require ( "fs" ) ;
- moment  const =  exiger ( "moment" ) ;
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const moment = require("moment");
 
 
-client . commandes  =  nouveau  Discord . Collection ( ) ;
-client . alias  =  nouveau  Discord . Collection ( ) ;
+client.commandes = new Discord.Collection();
+client.alias = new Discord.Collection();
 
 
 
 // Handler pour commandes
-fs . readdir ( "./commands/" ,  ( erreur ,  f )  =>  {
-    si ( erreur )  console . log ( erreur ) ;
+fs.readdir("./commands/", (error, f) => {
+    if(error) console.warn(error);
 
-    laissez  commandes  =  f . filtre ( f  =>  f . split ( "." ) . pop ( )  ===  "js" ) ;
-    if ( commandes . length <= 0 )  retourne la  console . log ( "Aucune commande trouvée!" ) ;
+	let commandes = f.filter(f => f.split(".").pop() === "js")
+    if(commandes.length === 0) console.warn("Aucune commande trouvée!")
 
-    commandes . forEach ( ( f )  =>  {
-        laissez  commande  =  require ( `./commands/ $ { f } ` ) ;
-        console . log ( ` $ { f } chargé!` ) ;
+    commandes.forEach(f => {
+		let commande = require(`./commands/${f}`);
+		console.log(`${f} chargé!`)
 
-        client . commandes . set ( commande . help . name ,  commande ) ;
+        client.commandes.set(commande.help.name, commande);
 
-        if ( commande . help . alias )  {
-        commande . aide . alias . forEach ( alias  =>  {
-            client . alias . set ( alias ,  commande . help . name ) ;
-        } ) } ;
-    } ) ;
-} ) ;
+        if(commande.help.alias){
+        commande.help.alias.forEach(alias => {
+            client.alias.set(alias, commande.help.name);
+        })};
+    });
+});
 
 
 // Handler pour les événements
-fs . readdir ( "./events/" ,  ( erreur ,  f )  =>  {
-    si ( erreur )  console . log ( erreur ) ;
-    console . log ( ` $ { f . length } chargés d'événements` ) ;
-
-    f . forEach ( ( f )  =>  {
-         événements  const =  require ( `./events/ $ { f } ` ) ;
-         événement  const =  f . split ( "" ) [ 0 ] ;
-
-    client . on ( événement ,  événements . bind ( null ,  client ) ) ;
-    } ) ;
-} ) ;
+fs.readdir("./events", (error, f) => {
+	if(error) console.warn(error);
+	console.log(`${f.length} évènements chargés!`)
+	f.forEach(f => {
+		const events = require(`./events/${f}`);
+		const event = f.split("")[0];
+	})
+	client.on(event, events.bind(null, client));
+});
  
- 
- client . connexion ( "NzE2OTU4NzM3MTg5MDQ0Mjg0.XwIuhA.v1q5xb5Fnh1teB_z3toLJT5-DYs" ) ;
+client.log("NzE2OTU4NzM3MTg5MDQ0Mjg0.XwIuhA.v1q5xb5Fnh1teB_z3toLJT5-DYs");
 
-
-client . on ( "ready" ,  ( )  =>  console . log ( "connecté" ) ) ;
-client . on ( "ready" , ( )  =>  client . utilisateur . setActivity ( "Salut!" ) ) ;
+client.on("ready", () => {
+	console.log(`Connecté!`)
+	bot.user.setActivity("Salut!")
+})
--- documents système/message.js
+++ documents système/message.js
@@ -1,6 +1,6 @@
-module.exports.run = asynchrone (client, message) => 
+module.exports.run = async(client, message) => 
 
-préfixe const = "/";
+const prefix = "/"
 
 if (message.channel.type === "dm") return;
 if (! message.content.startsWith (prefix)) return;
@@ -8,11 +8,11 @@ if (! message.author.bot) return;
 if (! message.guild) return;
 
 
-const args = message.content.splice (prefix.length) .trim (). split (/ + / g);
-const commande = args.shift (). toLowerCase ();
-const cmd = client.commands.get (commande) || client.commands.get (client.aliases.get (commande))
-if (! cmd) retourne;
+const args = message.content.splice(prefix.length).trim().split(/ + / g);
+const commande = args.shift().toLowerCase();
+const cmd = client.commands.get(commande) || client.commands.get(client.aliases.get (commande))
+if (! cmd) return;
 
-cmd.run (client, message, arguments);
+cmd.run (client, message, args);
 
 };
