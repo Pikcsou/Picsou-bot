221af7e35e9e395c2bf8374901f38a8e0c4f680d
Pikcsou <69587870+Pikcsou@users.noreply.github.com>
2021-02-06 15:04:35 +0100

    Update message.js

--- documents système/message.js
+++ documents système/message.js
@@ -1,18 +1,32 @@
-module.exports.run = asynchrone (client, message) => 
+const prefix = "/";
+const { readFileSync } = require("fs");
+const { MessageEmbed } = require("discord.js");
+module.exports = async(client, message) => {
+    if(!message.guild) return;
+    if(message.channel.type === "dm") return;
+    
+    const fichier = JSON.parse(readFileSync("./commands/serveur.json"));
+    if(!fichier[message.guild.id]) { fichier[message.guild.id] = {}; };
 
-préfixe const = "/";
+    let Prefix;
+    if(!fichier[message.guild.id].prefix) { Prefix = prefix; } else { Prefix = fichier[message.guild.id].prefix; };
 
-if (message.channel.type === "dm") return;
-if (! message.content.startsWith (prefix)) return;
-if (! message.author.bot) return;
-if (! message.guild) return;
+    const embed = new MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(`Mon préfixe est \`${Prefix}\` \n Exécutez la commande \`${Prefix}help\` pour obtenir la liste de mes commandes !`)
+    .setTimestamp()
+    if(message.mentions.users.has(client.user.id)) message.channel.send(embed).then(m => m.delete({timeout : 10000}));
+    
+    if(!message.content.startsWith(Prefix)) return;
+    if(message.author.bot) return;
 
+    const args = message.content.slice(Prefix.length).trim().split(/ +/g);
+    const commande = args.shift().toLowerCase();
+    const cmd = client.commands.get(commande) || client.commands.get(client.aliases.get(commande));
+    if(!cmd) return;
+    
+    const blockedUsers = [""];
+    if(blockedUsers.includes(message.author.id)) return message.reply("vous avez été blacklisté. En cas de contestations, veuillez contacter le support.");
 
-const args = message.content.splice (prefix.length) .trim (). split (/ + / g);
-const commande = args.shift (). toLowerCase ();
-const cmd = client.commands.get (commande) || client.commands.get (client.aliases.get (commande))
-if (! cmd) retourne;
-
-cmd.run (client, message, arguments);
-
+    cmd.run(client, message, args);
 };
